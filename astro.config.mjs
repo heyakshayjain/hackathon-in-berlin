9d2d4bc9d1a05c2d42eaa36ba418312d568127c3
Akshay Jain <114152892+heyakshayjain@users.noreply.github.com>
2025-07-05 14:29:12 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,6 +14,7 @@ export default defineConfig({
 					items: [
 						// Each item here is one entry in the navigation menu.
 						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: 'Best SUVs in 2025', link: '/best-suv-in-2025/' },
 					],
 				},
 				{
